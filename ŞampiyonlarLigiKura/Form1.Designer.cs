823232b4a1795737473322f8f7889247dcd97f1f
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-10-28 16:44:30 +0300

    Add files via upload

---
+++ ŞampiyonlarLigiKura/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace ŞampiyonlarLigiKura
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.listBox9 = new System.Windows.Forms.ListBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.listBox10 = new System.Windows.Forms.ListBox();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.listBox11 = new System.Windows.Forms.ListBox();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.listBox12 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(239, 88);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(195, 199);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(459, 88);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(195, 199);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(680, 88);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(195, 199);
+            this.listBox4.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.listBox5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 326);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 174);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "A Grubu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(65, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1.Torba";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(297, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2.Torba";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(517, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3.Torba";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(740, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4.Torba";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(7, 20);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(182, 147);
+            this.listBox5.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox2.Controls.Add(this.listBox6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(239, 326);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(195, 174);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "B Grubu";
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(7, 20);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(182, 147);
+            this.listBox6.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox3.Controls.Add(this.listBox7);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(459, 326);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(195, 174);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "C Grubu";
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(7, 20);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(182, 147);
+            this.listBox7.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox4.Controls.Add(this.listBox8);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox4.Location = new System.Drawing.Point(680, 326);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(195, 174);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "D Grubu";
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.Location = new System.Drawing.Point(7, 20);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(182, 147);
+            this.listBox8.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox5.Controls.Add(this.listBox9);
+            this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox5.Location = new System.Drawing.Point(13, 525);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(195, 174);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "E Grubu";
+            // 
+            // listBox9
+            // 
+            this.listBox9.FormattingEnabled = true;
+            this.listBox9.Location = new System.Drawing.Point(7, 20);
+            this.listBox9.Name = "listBox9";
+            this.listBox9.Size = new System.Drawing.Size(182, 147);
+            this.listBox9.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox6.Controls.Add(this.listBox10);
+            this.groupBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox6.Location = new System.Drawing.Point(239, 525);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(195, 174);
+            this.groupBox6.TabIndex = 13;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "F Grubu";
+            // 
+            // listBox10
+            // 
+            this.listBox10.FormattingEnabled = true;
+            this.listBox10.Location = new System.Drawing.Point(7, 20);
+            this.listBox10.Name = "listBox10";
+            this.listBox10.Size = new System.Drawing.Size(182, 147);
+            this.listBox10.TabIndex = 0;
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox7.Controls.Add(this.listBox11);
+            this.groupBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox7.Location = new System.Drawing.Point(459, 525);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(195, 174);
+            this.groupBox7.TabIndex = 14;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "G Grubu";
+            // 
+            // listBox11
+            // 
+            this.listBox11.FormattingEnabled = true;
+            this.listBox11.Location = new System.Drawing.Point(7, 20);
+            this.listBox11.Name = "listBox11";
+            this.listBox11.Size = new System.Drawing.Size(182, 147);
+            this.listBox11.TabIndex = 0;
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox8.Controls.Add(this.listBox12);
+            this.groupBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox8.Location = new System.Drawing.Point(680, 525);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(195, 174);
+            this.groupBox8.TabIndex = 15;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "H Grubu";
+            // 
+            // listBox12
+            // 
+            this.listBox12.FormattingEnabled = true;
+            this.listBox12.Location = new System.Drawing.Point(7, 20);
+            this.listBox12.Name = "listBox12";
+            this.listBox12.Size = new System.Drawing.Size(182, 147);
+            this.listBox12.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(666, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 26);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "TORBA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(778, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 26);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "GRUPLAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(893, 711);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox8);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Şampiyonlar Ligi Kura Çekim";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox8.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox listBox9;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.ListBox listBox10;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.ListBox listBox11;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.ListBox listBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ŞampiyonlarLigiKura/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ŞampiyonlarLigiKura
+{
+    public partial class Form1 : Form
+    {
+        //List ögesi verileri hafızada tutmaya yarar ve onları listeler.
+        List<Takım> takimlar;
+        List<ListBox> torbalar = new List<ListBox>();
+        List<ListBox> gruplar = new List<ListBox>(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Takım yenitakim = null;
+
+            takimlar = new List<Takım>();
+
+            yenitakim= new Takım ("Liverpool", "İngiltere");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Manchester City", "İngiltere");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Barcelona", "İspanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Bayern Münih", "Almanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Paris Saint Germain", "Fransa");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Real Madrid", "İspanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Chelsea", "İngiltere");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Manchester United", "İngiltere");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Atletico Madrid", "İspanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Juventus", "İtalya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Inter", "İtalya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Borussia Dortmund", "Almanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("RasenBallsport Leipzig", "Almanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Atalanta", "İtalya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Lazio", "İtalya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Sevilla", "İspanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Borussia Mönchengladbach", "Almanya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Ajax Amsterdam", "Hollanda");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Porto", "Portekiz");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Stade Rennes", "Fransa");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Olimpik Marsilya", "Fransa");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Zenit", "Rusya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Brügge", "Belçika");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Red Bull Salzburg", "Avusturya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Skakhtar Donetsk", "Avusturya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("FK Krasnodar", "Rusya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Dynamo Kyiv", "Ukrayna");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Olimpiakos Pire", "Yunanistan");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("İstanbul Başakşehir", "Türkiye");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Lokomotif Moskova", "Rusya");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("FC Midtjylland", "Danimarka");
+            takimlar.Add(yenitakim);
+
+            yenitakim = new Takım("Ferencvarosi TC", "Macaristan");
+            takimlar.Add(yenitakim);
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> secilentakimlar = new List<int>();
+            for (int j = 0; j < 4; j++)
+            {
+                secilentakimlar.Clear(); //aynı takımlar seçilmesin.
+                for (int i = 0; i < 8; i++)
+                {
+                    int secilentakim = rnd.Next(0, takimlar.Count / 4); //her torbaya 8 tane takım rastgele gidecek.
+
+                    if (secilentakimlar.Contains(secilentakim))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        secilentakimlar.Add(secilentakim);
+                    }
+                }
+                //aynı ülke takımları aynı gruba düşmesin.
+                bool ayniulkedentakimvar = false;
+                for (int k = 0; k < 8; k++)
+                {
+                    ayniulkedentakimvar = AynıUlkedenTakimVarMi(gruplar[k], torbalar[j].Items[secilentakimlar[k]] as Takım);
+                    if (ayniulkedentakimvar)
+                    {
+                        break;
+                    }
+
+                }
+                if (!ayniulkedentakimvar) //eğer aynı ülkeden takım yoksa o zaman listboxlara ekle.
+                {
+                    listBox1.Items.Add(torbalar[j].Items[secilentakimlar[0]] as Takım);
+                    listBox2.Items.Add(torbalar[j].Items[secilentakimlar[1]] as Takım);
+                    listBox3.Items.Add(torbalar[j].Items[secilentakimlar[2]] as Takım);
+                    listBox4.Items.Add(torbalar[j].Items[secilentakimlar[3]] as Takım);
+                    listBox5.Items.Add(torbalar[j].Items[secilentakimlar[4]] as Takım);
+                    listBox6.Items.Add(torbalar[j].Items[secilentakimlar[5]] as Takım);
+                    listBox7.Items.Add(torbalar[j].Items[secilentakimlar[6]] as Takım);
+                    listBox8.Items.Add(torbalar[j].Items[secilentakimlar[7]] as Takım);
+                }
+                else
+                {
+                    j--;
+                }
+            }
+        }
+                private bool AynıUlkedenTakimVarMi(ListBox grup, Takım takim)
+                {
+                    bool durum = false;
+                    for (int i = 0; i < grup.Items.Count; i++)
+                    {
+                        Takım gruptakim = grup.Items[i] as Takım;
+                        if (gruptakim.CountryName==takim.CountryName)
+                {
+                    durum = true;
+                    break;
+                }
+                }
+            return durum;
+                }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> seçilentakimlar = new List<int>();
+            for(int i=0;i<takimlar.Count;i++)
+            {
+                int seçilentakim = rnd.Next(0, takimlar.Count);
+                if(seçilentakimlar.Contains(seçilentakim))
+                {
+                    i--;
+                }
+                else
+                {
+                    seçilentakimlar.Add(seçilentakim);
+                }
+            }
+            for(int i=0;i<seçilentakimlar.Count;i++)
+            {
+                if(i<8)
+                {
+                    listBox1.Items.Add(takimlar[seçilentakimlar[i]]);
+                }
+                else if(i<16)
+                {
+                    listBox2.Items.Add(takimlar[seçilentakimlar[i]]);
+                }
+                else if (i < 24)
+                {
+                    listBox3.Items.Add(takimlar[seçilentakimlar[i]]);
+                }
+                else 
+                {
+                    listBox4.Items.Add(takimlar[seçilentakimlar[i]]);
+                }
+            }
+            torbalar.Add(listBox1);
+            torbalar.Add(listBox2);
+            torbalar.Add(listBox3);
+            torbalar.Add(listBox4);
+
+            gruplar.Add(listBox1);
+            gruplar.Add(listBox2);
+            gruplar.Add(listBox3);
+            gruplar.Add(listBox4);
+            gruplar.Add(listBox5);
+            gruplar.Add(listBox6);
+            gruplar.Add(listBox7);
+            gruplar.Add(listBox8);
+        }
+    }
+        }
+ 
